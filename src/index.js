88300676c61d77359cd6511fb97bfdfa1905e8ac
MarynaSB <111016765+MarynaSB@users.noreply.github.com>
2022-09-20 21:09:09 +0300

    Refactored event handlers

--- src/index.js
+++ src/index.js
@@ -44,106 +44,62 @@ function formatDate(currentDate) {
 updateDate(formatDate(new Date()));
 
 
-let isFahrenheit = false;
-let currentTemperatureMin;
-let currentTemperatureMax;
-let currentCity = '';
-
-function searchCity (event) {
-  event.preventDefault();
-  getCityTemperature();
+function displayWeatherCondition(response) {
+  let tempMax = Math.round(response.data.main.temp_max);
+  if (tempMax > 0) tempMax = `+${tempMax}°C`;
+  else tempMax = `${tempMax}°C`;
+  let tempMin = Math.round(response.data.main.temp_min);
+  if (tempMin > 0) tempMin = `+${tempMin}°C`;
+  else tempMin = `${tempMin}°C`;
+  document.querySelector(".actual-city").innerHTML = response.data.name;
+  document.querySelector(
+    ".actual-temperature"
+  ).innerHTML = `${tempMax}${"\u00A0"}${"\u00A0"}${tempMin}`;
+  document.querySelector(".description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
-function getCityTemperature() {
-  let cityInput = document.querySelector("#input-city");
-  let actualCity = document.querySelector(".actualCity");
-  if (cityInput.value) currentCity = cityInput.value;
-  if (currentCity) {
-    actualCity.innerHTML = currentCity;
-    callApi(currentCity);
-  } else {
-    currentCity = null;
-    actualCity.innerHTML = null;
-    alert("Please type a city");
-  }
+function searchCity(city) {
+  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#input-city").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+  let units = "metric";
+  // if (isFahrenheit) units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 function linkClick(event, city) {
   event.preventDefault();
-  let cityInput = document.querySelector("#input-city");
-  cityInput.value = null;
   currentCity = city;
-  let actualCity = document.querySelector(".actualCity");
-  if (currentCity) {
-    actualCity.innerHTML = currentCity;
-    callApi(currentCity);
-  }
+  document.querySelector(".actual-city").innerHTML = currentCity;
+  searchCity(currentCity);
 }
 
-function geoLocationClick(position) {
-  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let units = "metric";
-  if (isFahrenheit) units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
- 
-  axios.get(apiUrl).then(function (response) {
-    setTemp(response);
-     let actualCity = document.querySelector(".actualCity");
-      currentCity = response.data.name;
-      actualCity.innerHTML = currentCity;
-      let cityInput = document.querySelector("#input-city");
-      cityInput.value = "";
-  });
-}
-
-  function getCurrentPosition () {
-    navigator.geolocation.getCurrentPosition(geoLocationClick);
-  }
-
-function callApi(city) {
-  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
-  let units = "metric";
-  if (isFahrenheit) units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-
-  axios.get(apiUrl).then(function (response) {
-    setTemp(response);
-  });
-}
-
-function setTemp (response) {
-currentTemperatureMin = Math.round(response.data.main.temp_min);
-currentTemperatureMax = Math.round(response.data.main.temp_max);
-let currentTemperature = document.querySelector(".actual-temperature");
-let degreeSign = "°C";
-if (isFahrenheit) {
-  degreeSign = "°F";
-}
-currentTemperature.innerHTML = `${temperatureSign(
-  currentTemperatureMax
-)}${currentTemperatureMax}${degreeSign}  ${temperatureSign(
-  currentTemperatureMin
-)}${currentTemperatureMin}${degreeSign}`;
-}
-
-function temperatureSign(temperature) {
-  if (temperature > 0) return "+";
-  return "";
-}
-
-function convert(event) {
-  event.preventDefault();
-  isFahrenheit = !isFahrenheit;
-  getCityTemperature();
-}
-
-let fahrenheit = document.querySelector(".degrees");
-fahrenheit.addEventListener("click", convert);
+let searchCityForm = document.querySelector("#search-city");
+searchCityForm.addEventListener("submit", handleSubmit);
 
 let currentButton = document.querySelector(".current-location");
 currentButton.addEventListener("click", getCurrentPosition);
 
-let searchCityForm = document.querySelector("#search-city");
-searchCityForm.addEventListener("submit", searchCity);
+searchCity("Barcelona");
