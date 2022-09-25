e754a247f6509ae40bcdf1ec2062d6f8fadb9f7b
MarynaSB <111016765+MarynaSB@users.noreply.github.com>
2022-09-25 22:43:54 +0300

    Updated icon maping

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-    let days = [
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -31,7 +30,7 @@ function formatDate(timestamp) {
   hours = hours > 9 ? hours : "0" + hours;
   let minutes = date.getMinutes();
   minutes = minutes > 9 ? minutes : "0" + minutes;
-  return (`${day}, ${month} ${currentDate}, ${hours}:${minutes}`);
+  return `${day}, ${month} ${currentDate}, ${hours}:${minutes}`;
 }
 
 function displayWeatherCondition(response) {
@@ -53,8 +52,7 @@ function displayWeatherCondition(response) {
     if (tempMin > 0) tempMin = `+${tempMin}°F`;
     else tempMin = `${tempMin}°F`;
   }
-
-  let iconElement = document.querySelector("#weather-icon");
+  getWeatherIcon(response);
   document.querySelector("#actual-city").innerHTML = response.data.name;
   document.querySelector(
     "#actual-temperature"
@@ -66,13 +64,54 @@ function displayWeatherCondition(response) {
     response.data.wind.speed
   );
   document.querySelector("#actual-date-time").innerHTML = formatDate(
-    response.data.dt * 1000);
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    response.data.dt * 1000
+  );
+}
+
+function getWeatherIcon(response) {
+  let iconElement = document.querySelector("#weather-icon");
+  if (response.data.weather[0].icon === "01d") {
+    iconElement.setAttribute("src", `images/01d.png`);
+  } else if (response.data.weather[0].icon === "01n") {
+    iconElement.setAttribute("src", `images/01n.png`);
+  } else if (response.data.weather[0].icon === "02d") {
+    iconElement.setAttribute("src", `images/02d.png`);
+  } else if (response.data.weather[0].icon === "02n") {
+    iconElement.setAttribute("src", `images/02n.png`);
+  } else if (response.data.weather[0].icon === "03d") {
+    iconElement.setAttribute("src", `images/03d.png`);
+  } else if (response.data.weather[0].icon === "03n") {
+    iconElement.setAttribute("src", `images/03n.png`);
+  } else if (response.data.weather[0].icon === "04d") {
+    iconElement.setAttribute("src", `images/04d.png`);
+  } else if (response.data.weather[0].icon === "04n") {
+    iconElement.setAttribute("src", `images/04n.png`);
+  } else if (response.data.weather[0].icon === "09d") {
+    iconElement.setAttribute("src", `images/09d.png`);
+  } else if (response.data.weather[0].icon === "09n") {
+    iconElement.setAttribute("src", `images/09n.png`);
+  } else if (response.data.weather[0].icon === "10d") {
+    iconElement.setAttribute("src", `images/10d.png`);
+  } else if (response.data.weather[0].icon === "10n") {
+    iconElement.setAttribute("src", `images/10n.png`);
+  } else if (response.data.weather[0].icon === "11d") {
+    iconElement.setAttribute("src", `images/11d.png`);
+  } else if (response.data.weather[0].icon === "11n") {
+    iconElement.setAttribute("src", `images/11n.png`);
+  } else if (response.data.weather[0].icon === "13d") {
+    iconElement.setAttribute("src", `images/13d.png`);
+  } else if (response.data.weather[0].icon === "13n") {
+    iconElement.setAttribute("src", `images/13n.png`);
+  } else if (response.data.weather[0].icon === "50d") {
+    iconElement.setAttribute("src", `images/50d.png`);
+  } else if (response.data.weather[0].icon === "50n") {
+    iconElement.setAttribute("src", `images/50n.png`);
   }
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
 
 function searchCity(city) {
-  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
   axios.get(apiUrl).then(displayWeatherCondition);
@@ -85,7 +124,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "ac209dae1f283fb332a5bb7f50b0f468";
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${units}&appid=${apiKey}`;
   axios.get(apiUrl).then(displayWeatherCondition);
@@ -112,7 +151,7 @@ function convert(event) {
   displayWeatherCondition(currentApiResponse);
 }
 
-function getFahrenheitTemperature (celsiusTemperature) {
+function getFahrenheitTemperature(celsiusTemperature) {
   return (celsiusTemperature * 9) / 5 + 32;
 }
 
